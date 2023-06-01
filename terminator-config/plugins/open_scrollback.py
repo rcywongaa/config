a9590bc0965ba573543cae43ff79c43175ca2a71
Rufus Wong <rcywongaa@gmail.com>
2023-06-01 15:40:47 +0200

    Sync terminator settings

--- terminator-config/plugins/open_scrollback.py
+++ terminator-config/plugins/open_scrollback.py
@@ -7,19 +7,29 @@ AVAILABLE = ['OpenScrollback']
 
 class OpenScrollback(plugin.Plugin):
     def __init__(self):
-        self.current_window = Terminator().windows[0]
-        self.current_window.connect('key-press-event', self.onKeyPress)
+        # super().__init__(self)
+        self.windows = Terminator().get_windows()
+        for window in self.windows:
+            window.connect('key-press-event', self.onKeyPress)
 
+    # def onNewWindow(self):
     def onKeyPress(self, widget, event):
         if (event.state & Gdk.ModifierType.MOD1_MASK == Gdk.ModifierType.MOD1_MASK) and (event.keyval == 32): # Alt+Space
-            current_terminal = self.current_window.get_focussed_terminal()
+            # current_terminal = Terminator().get_focussed_terminal() # doesn't work for some reason
+            current_terminal = Terminator().last_focused_term
+            if current_terminal is None:
+                print("No focused terminal!")
+                return
+            else:
+                print("Found focused terminal!")
+            current_window = current_terminal.get_toplevel()
             vte = current_terminal.get_vte()
             col, row = vte.get_cursor_position()
             content, _ = vte.get_text_range(0, 0, row, col, lambda *a: True)
             temp_file = tempfile.NamedTemporaryFile(delete=False)
             with open(temp_file.name, 'w') as f:
                 f.write(content)
-            tab = self.current_window.tab_new()
-            new_terminal = self.current_window.get_focussed_terminal()
+            tab = current_window.tab_new()
+            new_terminal = current_window.get_focussed_terminal()
             command = " vim " + temp_file.name + '\n'
             new_terminal.vte.feed_child(str(command).encode("utf-8"))
